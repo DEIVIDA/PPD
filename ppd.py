939812e85a0af86567ea992d1229d0e85df06db1
drabulis <naikintuvas111@gmail.com>
2023-08-18 15:38:52 +0300

    abababa

---
+++ ppd.py
@@ -0,0 +1,16 @@
+saldytuvas = {"slyvos" : "3"}
+while True:
+    pasirinkimas = input("pasirinkit:")
+    if pasirinkimas == "0":
+        break
+    
+    elif pasirinkimas == "1": # Deivida
+        pass
+    elif pasirinkimas == "2": # Gabrielius
+        pass
+    elif pasirinkimas == "3": # Giedrius
+        pass
+    elif pasirinkimas == "4": # Mindaugas
+        pass
+    elif pasirinkimas == "5": # Ruslanas
+        pass
