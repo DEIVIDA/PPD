4a20cae55475c1f38c3dcd44a1bf21ce82eb5a4c
Niazaris <giedrius954@gmail.com>
2023-08-21 09:32:23 +0300

    Istraukimas

--- ppd.py
+++ ppd.py
@@ -1,15 +1,25 @@
-saldytuvas = {"slyvos" : "3"}
+saldytuvas = {"slyvos" : 3}
 while True:
-    pasirinkimas = input("pasirinkit:")
+    pasirinkimas = input("pasirinkit: ")
     if pasirinkimas == "0":
         break
-    
+
     elif pasirinkimas == "1": # Deivida
         pass
     elif pasirinkimas == "2": # Gabrielius
         pass
-    elif pasirinkimas == "3": # Giedrius
-        pass
+    elif pasirinkimas == "3": # Giedrius - istraukti produkta
+        istraukimas_produktas = str(input("pasirinkite produkta: "))
+        if not istraukimas_produktas in saldytuvas.keys():
+            print("produkto nera")
+        else:
+            istraukimas_kiekis = int(input("pasirinkite kieki: "))
+            if istraukimas_produktas in saldytuvas.keys() and saldytuvas[istraukimas_produktas] < istraukimas_kiekis:
+                print("tiek daug produkto nera")
+            if istraukimas_produktas in saldytuvas.keys() and saldytuvas[istraukimas_produktas] >= istraukimas_kiekis:
+                saldytuvas[istraukimas_produktas] -= istraukimas_kiekis
+            if saldytuvas[istraukimas_produktas] == 0:
+                saldytuvas.pop(istraukimas_produktas)                 
     elif pasirinkimas == "4": # Mindaugas
         pass
     elif pasirinkimas == "5": # Ruslanas
