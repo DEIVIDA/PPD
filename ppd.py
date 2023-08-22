9acac6642e41c3aa99175cd551ee8b8097ab119a
drabulis <naikintuvas111@gmail.com>
2023-08-22 11:33:54 +0300

    naujas saldytuvas

--- ppd.py
+++ ppd.py
@@ -1,6 +1,22 @@
+#1 
+def prideti():
+    pass
+#2
+def papildyti():
+    pass
+#3
+def skaiciuoti_turinio_svori():
+    pass
+#4
+def ar_iseina():
+    pass
+#5
+def turinys():
+    pass
+
 saldytuvas = {"slyvos": 3, "bananai" :5, "pasikorusi ziurke": 0.5, "kiausiniai": 7, }
 while True:
-    pasirinkimas = input("0 - išeiti iš šaldytuvo\n 1 - pridėti naują produktą\n 2 - papildyti produkto kiekį\n 3 - ištraukti produktą\n 4 - peržiūrėti produktus\n 5 - ieškoti produktų\n  pasirinkite:")
+    pasirinkimas = input("0 - išeiti iš šaldytuvo\n1 - pridėti naują produktą\n2 - papildyti produkto kiekį\n3 - ištraukti produktą\n4 - peržiūrėti produktus\n5 - ieškoti produktų\n  pasirinkite:")
     if pasirinkimas == "0":
         break      
     elif pasirinkimas == "1": # Deivida
