24d18c753182fa16acdbfb2bb7b718ea05f61a55
drabulis <naikintuvas111@gmail.com>
2023-08-28 13:28:35 +0300

    json saldytuvas

--- ppd.py
+++ ppd.py
@@ -1,35 +1,5 @@
-'''
-Parašykite programą šaldytuvas, kuri:
+import json
 
-- saugo produktus žodyne, kur:
--- produkto pavadinimas yra raktas
--- kiekis yra reikšmė float
-
-Tarkime, kad:
-- kieti produktai matuojami kilogramais
-- skysti litrais
-- paruošti patiekalai matuojami vienetais
-tokiu atveju, mums nereikia apibrėžti mato vieneto
-
-Šaldytuvas turi meniu, per kurį galime:
-- įdėti produktus
-- išimti produktus
--- jeigu produkto kiekis tampa 0, ištriname
-- peržiūrėti produktų sąrašą
-- suskaičiuoti šaldytuvo turinio svorį
--- skystų tankis = 1
--- paruoštų patiekalų porcija sveria 1kg
-
-BONUS:
-Įterpti meniu punktą "ar išeina", kur vartotojo prašo įvesti:
-- vienos porcijos receptą
--- įvedimo formatas: "ingredientas: kiekis" vienoje eilutėje.
-- porcijų kiekį
-Tada programa turėtų:
-- išvesti, ar užtenka ingredientų patiekalams paruošti
-- jeigu neužtenka, išvardinti ko ir kiek trūksta (shopping list).
-- išvesti, kelioms porcijoms užtenka ingredientų, jei yra perteklius
-'''
 #3 Istraukti 
 def istraukti_produkta(saldytuvas):
     istraukimas_produktas = input("pasirinkite produkta: ")
@@ -102,22 +72,25 @@ def ieskoti_produktu(saldytuvas):
     
     
     
-saldytuvas = {"slyvos":3,"bananai":5,"pasikorusi ziurke":0.5,"kiausiniai":7,"pienas":2,"vanduo":4,"keciupas":1.2}
-while True:
-    pasirinkimas = input("0 - išeiti iš šaldytuvo\n1 - Prideti nauja ir papildyti produktus\n2 - Skaiciuoti saldytuvo svori\n3 - ištraukti produktą\n4 - peržiūrėti produktus\n5 - ieškoti produktų\n6 - Receptas\npasirinkite:")
-    if pasirinkimas == "0":
-        print("Uzdarete saldytuva!")
-        break
-    elif pasirinkimas == "1": # Gabrielius
-        print(papildyti(saldytuvas))
-    elif pasirinkimas == "3": # Giedrius - istraukti produkta
-        print(f"Istrauktas produktas: {istraukti_produkta(saldytuvas)}")            
-    elif pasirinkimas == "4": # Mindaugas
-        print(turinys(saldytuvas))
-    elif pasirinkimas == "5": # Ruslanas
-        ieskoti_produktu(saldytuvas)
-    elif pasirinkimas == "6":
-        ar_iseina(saldytuvas)
-    elif pasirinkimas == "2":
-        print(f"Šaldytuvo svoris: {skaiciuoti_turinio_svori(saldytuvas)}...")
-        print("\n")
+with open("saldytuvo_turinys.json", "r", encoding="utf-8") as saldytuvo_turinys:
+    saldytuvas = json.load(saldytuvo_turinys)
+    while True:
+        pasirinkimas = input("0 - išeiti iš šaldytuvo\n1 - Prideti nauja ir papildyti produktus\n2 - Skaiciuoti saldytuvo svori\n3 - ištraukti produktą\n4 - peržiūrėti produktus\n5 - ieškoti produktų\n6 - Receptas\npasirinkite:")
+        if pasirinkimas == "0":
+            with open("saldytuvo_turinys.json", "w", encoding="utf-8") as saldytuvo_turinys:
+                json.dump(saldytuvas, saldytuvo_turinys)
+            print("Uzdarete saldytuva!")
+            break
+        elif pasirinkimas == "1": # Gabrielius
+            print(papildyti(saldytuvas))
+        elif pasirinkimas == "3": # Giedrius - istraukti produkta
+            print(f"Istrauktas produktas: {istraukti_produkta(saldytuvas)}")            
+        elif pasirinkimas == "4": # Mindaugas
+            print(turinys(saldytuvas))
+        elif pasirinkimas == "5": # Ruslanas
+            ieskoti_produktu(saldytuvas)
+        elif pasirinkimas == "6":
+            ar_iseina(saldytuvas)
+        elif pasirinkimas == "2":
+            print(f"Šaldytuvo svoris: {skaiciuoti_turinio_svori(saldytuvas)}...")
+            print("\n")
